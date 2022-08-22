0dea6f470dd1b5ec1d8cbf2790065efc7b7490ce
glensorbo <74546153+glensorbo@users.noreply.github.com>
2022-08-22 13:55:34 +0200

    Glen/feat: Adds redux toolkit (#29)

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -5,11 +5,17 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
