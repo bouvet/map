9d6cccd778a0a75e2952c30781f806c8eed71259
Sindre Reidar Mohr <69864128+SindreMohr@users.noreply.github.com>
2022-08-22 14:58:35 +0200

    sindre/feature styled components (#32)

--- spa/src/components/Map/MyMap.tsx
+++ spa/src/components/Map/MyMap.tsx
@@ -13,14 +13,12 @@ import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import { MyTheme } from '../../styles/global';
 import { features } from 'process';
-
 import { useStateDispatch } from '../../hooks/useRedux';
 import { mapService } from '../../services/map.services';
 
 
 const MyMap: FC = () => {
   const [map, setMap]: [map: any, setMap: any] = useState();
-
   const dispatch = useStateDispatch();
   const mapElement: any = useRef();
   const mapRef = useRef();
@@ -37,7 +35,6 @@ const MyMap: FC = () => {
     geometry: new Point(pos),
     name: 'My Marker',
   });
-
 
   const pin :string = `<svg width='33' height='53' version='1.1' xmlns='http://www.w3.org/2000/svg'><path d='M33 16.5C33 25.6127 28 33 16.5 53.5C5 33 0 25.6127 0 16.5C0 7.3873 7.3873 0 16.5 0C25.6127 0 33 7.3873 33 16.5Z' fill='${MyTheme.colors.accent.replace("#", "%23")}' /></svg>`
 
