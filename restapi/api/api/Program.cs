ac340470146bd901b6ecff00a67822e4cc66bdc0
Skjalg G. Eriksen <skjalg.eriksen@bouvet.no>
2022-08-22 15:56:58 +0200

    init setup api

---
+++ restapi/api/api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LocationContext>(opt =>opt.UseInMemoryDatabase("Location"));
+builder.Services.AddDbContext<CategoryContext>(opt =>
+    opt.UseInMemoryDatabase("Category"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
