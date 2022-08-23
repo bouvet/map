cba9f4db012ba8feccc277b84800cced1ec3bcbe
glensorbo <glensorbo@gmail.com>
2022-08-23 15:46:56 +0200

    Adds massive structure changes

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,31 +1,12 @@
-import Map from './components/Map/Map';
-import { FilterMenu } from './components/Filter/FilterMenu';
-import { FilterButton } from './components/Filter/Buttons';
-import { category } from './utils/types.d';
-import { useStateSelector } from './hooks/useRedux';
-import { Header } from './components/Navigation/Header';
-import { FilterEvent } from './utils/filterLogic';
+import { Home } from "./pages/Home";
 
-function App() {
 
-  const { selected } = useStateSelector(state => state.map)
-
-  const mappedFilter = category.map(item => (
-    <FilterButton key={item} text={item} />
-  ));
-  
-  FilterEvent();
+const App = () => {
 
   return (
-    <div className="App">
-      <Header content={selected}></Header>
-      <FilterMenu>
-        {mappedFilter}
+    <Home />
+  )
 
-      </FilterMenu>
-      <Map />
-    </div>
-  );
 }
 
 export default App;
---
+++ spa/src/features/map/index.ts
@@ -0,0 +1,2 @@
+export * from './components/Map';
+export * from './services/map.services';
