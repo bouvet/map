222cf75302cd1b7860bd309d4c9d3b25354ccf35
glensorbo <74546153+glensorbo@users.noreply.github.com>
2022-08-23 16:16:17 +0200

    Adds massive structure changes (#40)

--- spa/src/components/Filter/Buttons.tsx
+++ spa/src/components/Filter/Buttons.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MyTheme } from '../../styles/global';
 import { mapActions } from '../../store/state/map.state';
-import { useStateDispatch, useStateSelector } from '../../hooks/useRedux';
+import { useStateDispatch } from '../../hooks/useRedux';
 
 interface Props {
     clicked: boolean;
@@ -17,32 +17,28 @@ const FilterButtonStyle = styled.div<Props>`
     font-size: ${MyTheme.fontSize.body};
     padding: 0px 10px;
     border-radius: 27px;
-    background-color: ${props => props.clicked ? MyTheme.colors.accent : MyTheme.colors.darkbase };
+    background-color: ${(props) => (props.clicked ? MyTheme.colors.accent : MyTheme.colors.darkbase)};
     color: ${MyTheme.colors.lightbase};
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    `;
+`;
 
-
-
-export const FilterButton: React.FunctionComponent<FilterProps> = ({text}) => {
-
+export const FilterButton: React.FC<FilterProps> = ({ text }) => {
     const [select, setSelect] = useState(false);
 
+    const dispatch = useStateDispatch();
 
-    const dispatch = useStateDispatch()
-
-    const updateGlobalState = (activity :string) => {
+    const updateGlobalState = (activity: string) => {
         // Update the global state
-        console.log(activity);
+        // console.log(activity);
         dispatch(mapActions.setSelected(activity));
-    }
+    };
 
     const handleClick = () => {
         //setSelect(!select);
         updateGlobalState(text);
-    }
+    };
 
     return (
         <FilterButtonStyle clicked={select} onClick={handleClick}>
