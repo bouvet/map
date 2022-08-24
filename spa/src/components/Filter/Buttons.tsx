91658de7c9eda049b0121f73b992116c4ebc036f
Sindre Reidar Mohr <69864128+SindreMohr@users.noreply.github.com>
2022-08-24 11:27:55 +0200

    style: added fonts and fixed margins for demo (#46)

--- spa/src/components/Filter/Buttons.tsx
+++ spa/src/components/Filter/Buttons.tsx
@@ -4,32 +4,42 @@ import { MyTheme } from '../../styles/global';
 import { mapActions } from '../../store/state/map.state';
 import { useStateDispatch, useStateSelector } from '../../hooks/useRedux';
 
+/** For FilterButtonStyle to change colors based on whether the button is currently selected */
 interface Props {
+    /** State for checking if the buttons is currently selected */
     clicked: boolean;
 }
+/** Type for content of filterbutton */
 interface FilterProps {
+    /** Name of category */
     text: string;
 }
 
+
+/** Styling of filterbuttons, with conditional colors based on the Props.clicked value */
 const FilterButtonStyle = styled.div<Props>`
     box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
     height: 27px;
     font-size: ${MyTheme.fontSize.icon};
     padding: 0px 10px;
     border-radius: 27px;
-    background-color: ${(props) => (props.clicked ? MyTheme.colors.accent : MyTheme.colors.darkbase)};
+    background-color: ${props => props.clicked ? MyTheme.colors.accent : MyTheme.colors.darkbase };
     color: ${MyTheme.colors.lightbase};
     display: inline-flex;
     align-items: center;
     justify-content: center;
-`;
+    `;
 
-export const FilterButton: React.FC<FilterProps> = ({ text }) => {
 
+/** Filterbutton export with FilterProps.text as the displayed value of the button */
+export const FilterButton: React.FunctionComponent<FilterProps> = ({text}) => {
     const { selected } = useStateSelector(state => state.map)
     const [select, setSelect] = useState(false);
     const dispatch = useStateDispatch()
 
+    /** Updating the conditional coloring of the button based on the global state selected
+     *  Selected is a string with the currently selected category as a string.
+     */
     useEffect(() => {
         if (selected === text) {
             setSelect(true);
@@ -38,13 +48,16 @@ export const FilterButton: React.FC<FilterProps> = ({ text }) => {
         }
     }, [selected]);
 
-
+    /** Method for updating the global state selected when the button is clicked */
     const updateGlobalState = (activity :string) => {
         // Update the global state
-        // console.log(activity);
+        console.log(activity);
         dispatch(mapActions.setSelected(activity));
-    };
+    }
 
+    /** Event handler for click events.
+     *  Calls function for updating global state.
+     */
     const handleClick = () => {
         if (!select) {
             updateGlobalState(text);
@@ -52,7 +65,7 @@ export const FilterButton: React.FC<FilterProps> = ({ text }) => {
             updateGlobalState("");
         }
         setSelect(!select);
-    };
+    }
 
     return (
         <FilterButtonStyle clicked={select} onClick={handleClick}>
--- spa/src/components/Navigation/Header.tsx
+++ spa/src/components/Navigation/Header.tsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 import { MyTheme } from '../../styles/global';
 
+
+/** Header content as a string which will be structured as a H1 element */
 type HeaderContent = {
+    /** The stuff that will be converted  */
     content: string;
 }
 
+/** Header body, structure based on div
+ * Color set to be the light base color in the theme object
+ */
 const HeaderWrapper = styled.div`
     height: 130px;
     padding: 0px 10px;
@@ -15,11 +21,18 @@ const HeaderWrapper = styled.div`
     z-index: 1;
 `
 
+/** H1 Header for displaying name of application */
 const HeaderH1 = styled.h1`
     padding: 0px;
     margin: 0px;
+    color: ${MyTheme.colors.darkbase};
+    font-size: ${MyTheme.fontSize.header};
 `
 
+/** Header component with HeaderWrapper and H1 element
+ *  takes content passed as child in component usage.
+ *  content is set to be displayed as H1 element.
+ */
 export const Header: React.FunctionComponent<HeaderContent> = ({content}) => {
     return (
         <HeaderWrapper>
