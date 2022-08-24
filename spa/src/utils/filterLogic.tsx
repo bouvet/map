cdb3e22be75ba3ff00eb83733be2743c803e0389
Skjalg G. Eriksen <12465871+skjalg-eriksen@users.noreply.github.com>
2022-08-24 11:44:29 +0200

    doc: commented filter (#47)
    - added comments on types in type.d.tsx
    - added comments on functions in locationData.ts
    - added comments on functions in filterLogic.tsx

--- spa/src/utils/filterLogic.tsx
+++ spa/src/utils/filterLogic.tsx
@@ -2,13 +2,24 @@ import { useEffect } from 'react';
 import { useStateDispatch, useStateSelector } from '../hooks/useRedux';
 import { mapActions } from '../store/state/map.state';
 import { filter } from './locationData';
+import { Location } from './types.d';
 
+/**
+ *  sets up a useEffect()[@var selected] for filtering locations on "selected" state value
+ */
 export function FilterEvent() {
+
+
+    // redux stuff for global states
     const { selected, locations } = useStateSelector((state) => state.map);
     const dispatch = useStateDispatch();
 
     useEffect(() => {
-        // console.log('filer this ', selected, filter(locations, selected));
+        if (selected === ""){
+            dispatch(mapActions.setFilteredLocations(locations));    
+        }
+        // dispatch sets the global state value of filtered locations
         dispatch(mapActions.setFilteredLocations(filter(locations, selected)));
     }, [selected, dispatch, locations]);
+
 }
