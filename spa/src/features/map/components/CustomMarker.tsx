e2d9fc608f020156cdfb91a852177c9c838b902a
glensorbo <74546153+glensorbo@users.noreply.github.com>
2022-08-24 15:57:30 +0200

    fix: logic for changing color when marker is choosen (#51)

--- spa/src/features/map/components/CustomMarker.tsx
+++ spa/src/features/map/components/CustomMarker.tsx
@@ -1,23 +1,36 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Marker } from 'react-map-gl';
+import { MyTheme } from '../../../styles/global';
 import { Location } from '../../../utils/types.d';
 
 type Props = {
     coordinates: number[];
     onClickHandler: (obj: any) => void;
-    color?: string;
     style?: React.CSSProperties;
     locaction: Location;
+    selectedMarker: string;
 };
 
-export const CustomMarker: React.FC<Props> = ({ coordinates, onClickHandler, color, style, locaction }) => {
+export const CustomMarker: React.FC<Props> = ({ coordinates, onClickHandler, selectedMarker, locaction }) => {
+    const [color, setColor] = useState(MyTheme.colors.darkbase);
+
+    useEffect(() => {
+        if (selectedMarker === locaction.properties.title) {
+            setColor(MyTheme.colors.accent);
+        } else {
+            setColor(MyTheme.colors.darkcolor);
+        }
+    }, [selectedMarker, locaction.properties.title]);
+
     return (
-        <Marker
-            longitude={coordinates[0]}
-            latitude={coordinates[1]}
-            onClick={() => onClickHandler(locaction)}
-            anchor={'bottom'}
-            color={color}
-            style={style}
-        />
+        <Marker longitude={coordinates[0]} latitude={coordinates[1]} onClick={() => onClickHandler(locaction)} anchor={'bottom'}>
+            <svg width="30" height="35" viewBox="0 0 33 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M33 16.5C33 25.6127 28 33 16.5 53.5C5 33 0 25.6127 0 16.5C0 7.3873 7.3873 0 16.5 0C25.6127 0 33 7.3873 33 16.5Z"
+                    fill={color}
+                />
+            </svg>
+        </Marker>
     );
 };
