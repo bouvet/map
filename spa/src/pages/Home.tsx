d4e10dfa81482e0de18a530b53801c3dbde8f921
SindreMohr <sindre.mohr@bouvet.no>
2022-08-25 09:06:56 +0200

    updating stuff

--- spa/src/pages/Home.tsx
+++ spa/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 // import { useEffect } from 'react';
 import { FilterButton } from '../components/Filter/Buttons';
 import { FilterMenu } from '../components/Filter/FilterMenu';
+import { Popup } from '../components/Popup/Popup';
 // import { Header } from '../components/Navigation/Header';
 import { Map } from '../features/map';
 // import { useStateDispatch, useStateSelector } from '../hooks/useRedux';
@@ -24,6 +25,7 @@ export const Home = () => {
         <div className="App">
             {/* <Header content={selected}></Header> */}
             <FilterMenu>{mappedFilter}</FilterMenu>
+            <Popup name='Våland park' description='This park is lovely for people who like something to do' rating={3}></Popup>
             <Map />
         </div>
     );
