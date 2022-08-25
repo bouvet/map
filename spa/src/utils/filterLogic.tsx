fda08a281f56087b7c8fd43eb7aa72c2245c1a36
Skjalg G. Eriksen <12465871+skjalg-eriksen@users.noreply.github.com>
2022-08-25 14:08:42 +0200

    refactor: changed names and structure to increase readability (#53)
    - in types.d.tsx removed some comments
    - in locationData.ts changed filtered to applyFilterLocationOnCategory
    - in locationData.ts changed fake_locations to generateFakeLocations
    - in generateFakeLocations removed comments and added variables instead of directly adding fakerjs output into a Location object.
    - in filterLogic.ts changed FilterEvent to useFilterEvent
    - in filterLogic.ts removed comments, added a variable instead of directly using output from filtered to map action function.

--- spa/src/utils/filterLogic.tsx
+++ spa/src/utils/filterLogic.tsx
@@ -1,26 +1,21 @@
 import { useEffect } from 'react';
-import { mapService } from '../features/map';
 import { useStateDispatch, useStateSelector } from '../hooks/useRedux';
 import { mapActions } from '../store/state/map.state';
-import { filter } from './locationData';
-import { Location } from './types.d';
+import { applyFilterLocationOnCategory } from './locationData';
 
 /**
  *  sets up a useEffect()[@var selected] for filtering locations on "selected" state value
  */
-export function FilterEvent() {
-
-
-    // redux stuff for global states
+export function useFilterEvent() {
     const { selected, locations } = useStateSelector((state) => state.map);
     const dispatch = useStateDispatch();
 
     useEffect(() => {
-        // dispatch sets the global state value of filtered locations
         if (selected === ""){
             dispatch(mapActions.setFilteredLocations(locations));
         } else {
-            dispatch(mapActions.setFilteredLocations(filter(locations, selected)));
+            let filteredLocations = applyFilterLocationOnCategory(locations, selected)
+            dispatch(mapActions.setFilteredLocations(filteredLocations));
         }
     }, [selected, dispatch, locations]);
 
