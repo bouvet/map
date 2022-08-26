608832dd6f164cea79822f802599430dc02aeaa5
Glen Sørbø <74546153+glensorbo@users.noreply.github.com>
2022-08-26 09:40:54 +0200

    breaking: restapi restructure (#70)

--- restapi/Program.cs
+++ restapi/Program.cs
@@ -7,20 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LocationContext>(opt =>opt.UseInMemoryDatabase("Location"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LocationContext>(opt => opt.UseInMemoryDatabase("Location"));
 builder.Services.AddDbContext<CategoryContext>(opt =>
     opt.UseInMemoryDatabase("Category"));
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
