09c1b2e8c1e36c82d5237d0841c035cfabb9e7f6
Margrethe Amundsen <111041224+margretheamundsen@users.noreply.github.com>
2022-08-29 12:56:06 +0200

    added react router (#83)
    Co-authored-by: Sindre Reidar Mohr <69864128+SindreMohr@users.noreply.github.com>

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { LocationInfo } from './pages/LocationInfo';
+import { Login } from './pages/Login';
 
-const App = () => <Home />;
+const App = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/location-info" element={<LocationInfo />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    </Router>
+);
 
 export default App;
