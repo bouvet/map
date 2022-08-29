b53aacfcf2d91f94ca192832639af2e16abdcaca
Skjalg G. Eriksen <skjalg.eriksen@bouvet.no>
2022-08-29 10:14:19 +0200

    added category api without entity framework

---
+++ restapi/Controllers/CategoryController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace restapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private static List<Category> categories = new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Fotball",
+                    Emoji = "⚽"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Tennis",
+                    Emoji = "🎾"
+                }
+            };
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(categories); 
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> Get(int id)
+        {
+            var category = categories.Find(x => x.Id == id);
+            if (category == null) 
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Category>>> AddCategory(Category category)
+        {
+            categories.Add(category);
+            return Ok(categories);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Category>>> UpdateCategory(Category request)
+        {
+            var category = categories.Find(x => x.Id == request.Id);
+            if (category == null)
+                return NotFound();
+
+            category.Name = request.Name;
+            category.Emoji = request.Emoji;
+
+            
+            return Ok(categories);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Category>>> DeleteCategory(int id){
+            var category = categories.Find(x => x.Id == id);
+            if (category == null)
+                return NotFound("Categoy not found");
+            
+            categories.Remove(category);
+            return Ok(categories);
+        }
+
+    }
+}
