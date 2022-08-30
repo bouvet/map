edb3e8ee5a0b547202fc02a85597ef235162d253
Skjalg G. Eriksen <skjalg.eriksen@bouvet.no>
2022-08-30 13:43:20 +0200

    restapi category with ServiceResponse object and returning proper statuscodes

--- restapi/Controllers/CategoryController.cs
+++ restapi/Controllers/CategoryController.cs
@@ -16,91 +16,49 @@ namespace restapi.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<ServiceResponse<List<Category>>>> GetAllCategories()
         {
-            return Ok(await categoryService.GetAllCategories());
+            var response = await categoryService.GetAllCategories();
+            return StatusCode(response.StatusCode, response);
         }
+
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategory(int id)
         {
-            return Ok(await categoryService.GetCategory(id));
+            var response = await categoryService.GetCategory(id);
+            return StatusCode(response.StatusCode, response);
         }
 
         [HttpPost]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<List<CategoryDto>>> AddCategory(CategoryDto category)
         {
-            return Ok(await categoryService.AddCategory(category));
+            var response = await categoryService.AddCategory(category);
+            return StatusCode(response.StatusCode, response);
         }
 
 
         [HttpPut]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<List<Category>>> UpdateCategory(Category request)
         {
-            return Ok(await categoryService.UpdateCategory(request));
+            var response = await categoryService.UpdateCategory(request);
+            return StatusCode(response.StatusCode, response);
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<CategoryService>), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<List<Category>>> DeleteCategory(int id)
         {
-            return Ok(await categoryService.DeleteCategory(id));
+            var response = await categoryService.DeleteCategory(id);
+            return StatusCode(response.StatusCode, response);
         }
-        /*
-        private readonly DataContext _context;
-        public CategoryController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            return Ok(await _context.Categories.ToListAsync()); 
-        }
-        
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Category>> Get(int id)
-        {
-            //var category = categories.Find(x => x.Id == id);
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null) 
-                return NotFound();
-
-            return Ok(category);
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<List<Category>>> AddCategory(Category category)
-        {
-            _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Categories.ToListAsync());
-        }
-
-        [HttpPut]
-        public async Task<ActionResult<List<Category>>> UpdateCategory(Category request)
-        {
-            var category = await _context.Categories.FindAsync(request.Id);
-            if (category == null)
-                return NotFound();
-
-            category.Name = request.Name;
-            category.Emoji = request.Emoji;
-
-            await _context.SaveChangesAsync();
-            
-            return Ok(await _context.Categories.ToListAsync());
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<List<Category>>> DeleteCategory(int id){
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-                return NotFound();
-
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-            return Ok(await _context.Categories.ToListAsync());
-        }
-        */
     }
 }
