57a23f2f221bd21a21a1f1a5e346a81d7f308424
Glen Sørbø <74546153+glensorbo@users.noreply.github.com>
2022-09-07 13:11:55 +0200

    feat: ✨ adds user model, service and controller. (#147)
    Full CRUD functionality added. Buy now, save $3.99! 🥳

---
+++ restapi/Dtos/User/AddUserDto.cs
@@ -0,0 +1,11 @@
+namespace restapi.Dtos
+{
+  public class AddUserDto
+  {
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public int PostalCode { get; set; } = 0;
+    public string PostalArea { get; set; } = string.Empty;
+    public int BirthYear { get; set; } = 0;
+  }
+}
