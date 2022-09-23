02236cfd7b352d752f7b7d2d5c54dbbfc51cfd6b
Sindre Reidar Mohr <69864128+SindreMohr@users.noreply.github.com>
2022-09-23 13:19:16 +0200

    feat: ✨ adds fab menu and some fixes (#239)

--- spa/src/components/FabMenu/FabMenu.tsx
+++ spa/src/components/FabMenu/FabMenu.tsx
@@ -1,25 +1,14 @@
 import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useStateDispatch, useStateSelector } from '../../hooks/useRedux';
+import { authActions } from '../../store/state/auth.state';
 import { MyTheme } from '../../styles/global';
 import { GoogleIcon, RoundButton } from '../Navigation/Buttons';
 
-interface FabActiveProps {
-    active: boolean;
-}
-
-const Fab = styled(RoundButton)<FabActiveProps>`
-    transition: transform 500ms ease;
-    bottom: ${(active) => (active ? '10px' : '50px')};
+const Fab = styled(RoundButton)`
+    transition: transform 400ms ease;
 `;
-
-const MenuButton: FC = () => (
-    <RoundButton backgroundColor={MyTheme.colors.accent}>
-        <GoogleIcon color={MyTheme.colors.lightbase} className="material-symbols-outlined">
-            menu
-        </GoogleIcon>
-    </RoundButton>
-);
 
 export const FabMenu: FC = () => {
     const [isActive, setIsActive] = useState(false);
@@ -28,17 +17,77 @@ export const FabMenu: FC = () => {
         setIsActive(!isActive);
     };
 
+    const dispatch = useStateDispatch();
+
+    const { isAuthenticated } = useStateSelector((state) => state.auth);
+    console.log('auth: ', isAuthenticated);
+
+    const handleLogOut = () => dispatch(authActions.logOut());
+
     return (
         <>
-            <Fab backgroundColor={MyTheme.colors.lightbase} active={isActive}>
-                <Link to="/location-registration">
-                    <GoogleIcon color={MyTheme.colors.darkbase} className="material-symbols-outlined">
-                        add
-                    </GoogleIcon>
-                </Link>
-            </Fab>
+            {isAuthenticated ? (
+                <>
+                    <Fab
+                        backgroundColor={MyTheme.colors.lightbase}
+                        style={{
+                            transform: isActive ? 'translateY(-180px)' : 'translateY(0)',
+                            boxShadow: !isActive ? 'none' : '0px 0px 5px rgba(0, 0, 0, 0.25)',
+                        }}
+                    >
+                        <Link to="/">
+                            <GoogleIcon color={MyTheme.colors.darkbase} className="material-symbols-outlined">
+                                person
+                            </GoogleIcon>
+                        </Link>
+                    </Fab>
+                    <Fab
+                        backgroundColor={MyTheme.colors.lightbase}
+                        style={{
+                            transform: isActive ? 'translateY(-120px)' : 'translateY(0)',
+                            boxShadow: !isActive ? 'none' : '0px 0px 5px rgba(0, 0, 0, 0.25)',
+                        }}
+                    >
+                        <Link to="/location-registration">
+                            <GoogleIcon color={MyTheme.colors.darkbase} className="material-symbols-outlined">
+                                add
+                            </GoogleIcon>
+                        </Link>
+                    </Fab>
+                    <Fab
+                        backgroundColor={MyTheme.colors.lightbase}
+                        onClick={handleLogOut}
+                        style={{
+                            transform: isActive ? 'translateY(-60px)' : 'translateY(0)',
+                            boxShadow: !isActive ? 'none' : '0px 0px 5px rgba(0, 0, 0, 0.25)',
+                        }}
+                    >
+                        <GoogleIcon color={MyTheme.colors.darkbase} className="material-symbols-outlined">
+                            logout
+                        </GoogleIcon>
+                    </Fab>
+                </>
+            ) : (
+                <Fab
+                    backgroundColor={MyTheme.colors.lightbase}
+                    style={{
+                        transform: isActive ? 'translateY(-60px)' : 'translateY(0)',
+                        boxShadow: !isActive ? 'none' : '0px 0px 5px rgba(0, 0, 0, 0.25)',
+                    }}
+                >
+                    <Link to="/login">
+                        <GoogleIcon color={MyTheme.colors.darkbase} className="material-symbols-outlined">
+                            login
+                        </GoogleIcon>
+                    </Link>
+                </Fab>
+            )}
             <span onClick={handleClick} role="presentation">
-                <MenuButton />
+                <RoundButton backgroundColor={MyTheme.colors.accent}>
+                    <GoogleIcon color={MyTheme.colors.lightbase} className="material-symbols-outlined">
+                        {isActive ? 'close' : 'menu'}
+                    </GoogleIcon>
+                </RoundButton>
             </span>
         </>
     );
--- spa/src/features/login/components/LoginWrapper.tsx
+++ spa/src/features/login/components/LoginWrapper.tsx
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const LoginWrapper = styled.div`
     width: 100%;
     height: 100vh;
-    display: flex;
+    display: inline-flex;
     align-items: center;
     justify-content: center;
+    min-height: 570px;
 `;
 
 export const LoginContent = styled.div`
@@ -14,5 +15,5 @@ export const LoginContent = styled.div`
     max-width: 350px;
     display: flex;
     flex-direction: column;
-    overflow: scroll;
+    margin-top: -25px;
 `;
