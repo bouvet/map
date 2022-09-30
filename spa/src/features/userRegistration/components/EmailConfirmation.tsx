a7eec81b2f1fd0b0b5a1d238b3911e543a549878
margretheamundsen <margrethe.amundsen@bouvet.no>
2022-09-30 14:09:40 +0200

    a lot of updates

--- spa/src/features/userRegistration/components/EmailConfirmation.tsx
+++ spa/src/features/userRegistration/components/EmailConfirmation.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Dispatch, FC, FormEvent, useRef, useState } from 'react';
+import { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -14,39 +14,36 @@ import { LinkText, ProgressBarForm, Text, TitleForm } from '../../../components/
 import { BackButton } from '../../../components/Navigation/Buttons';
 import { MyTheme } from '../../../styles/global';
 import { Form } from '../../../components/Form/Form';
-import { SubmitButton } from '../../../components/Form/Buttons';
 
 export const EmailConfirmation: FC = () => {
     const navigate = useNavigate();
 
-    const inputRef = useRef();
+    const [inputCode, setInputCode] = useState<string[]>(new Array(6).fill(''));
 
-    const [inputCode, setInputCode] = useState('');
-
-    const handleFormInputChange = (e: ChangeEvent<HTMLInputElement>, setState: Dispatch<string>) => {
-        setState(e.target.value);
-    };
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        // add if-statement to validate code
-        e.preventDefault();
+    const handleSubmit = useCallback(() => {
         navigate('/personal-info');
-        console.log('Code: ', inputCode);
+    }, [navigate]);
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, index: number) => {
+        const tempArray = [...inputCode];
+        tempArray[index] = event.target.value;
+        setInputCode(tempArray);
+        const nextInput = document.querySelector(`input[name=input-field-${index + 1}]`) as HTMLElement | null;
+        if (nextInput !== null) {
+            nextInput.focus();
+        }
     };
+
+    useEffect(() => {
+        if (!inputCode.includes('')) {
+            handleSubmit();
+        }
+    }, [inputCode, handleSubmit]);
 
     const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        navigate('/user-registration');
-    };
-
-    const handleCloseDialog = () => {
-        setOpen(false);
-    };
+    const handleClickOpen = () => setOpen(true);
+    const handleClose = () => navigate('/user-registration');
+    const handleCloseDialog = () => setOpen(false);
 
     const pageIndex = 1;
 
@@ -73,35 +70,29 @@ export const EmailConfirmation: FC = () => {
             <FormContent>
                 <SectionWrapper>
                     <TitleForm>Bekreft e-post</TitleForm>
-                    <Form onSubmit={(e) => handleSubmit(e)}>
+                    <Form>
                         <ProgressBarForm pageIndex={pageIndex} />
                         <Text>Skriv inn koden for å bekrefte e-postadressen *fra EmailInput*</Text>
                         <Box
-                            component="form"
                             sx={{
                                 '& .MuiTextField-root': { m: '1%', width: '14%' },
                             }}
-                            noValidate
-                            autoComplete="off"
                         >
-                            <TextField
-                                id="outlined-number"
-                                type="tel"
-                                inputProps={{ maxLength: 1 }}
-                                autoFocus
-                                // value={inputCode}
-                                // @ts-ignore
-                                setState={setInputCode}
-                                handleChange={handleFormInputChange}
-                                inputRef={inputRef}
-                            />
-                            <TextField id="outlined-number" type="tel" inputProps={{ maxLength: 1 }} />
-                            <TextField id="outlined-number" type="tel" inputProps={{ maxLength: 1 }} />
-                            <TextField id="outlined-number" type="tel" inputProps={{ maxLength: 1 }} />
-                            <TextField id="outlined-number" type="tel" inputProps={{ maxLength: 1 }} />
-                            <TextField id="outlined-number" type="tel" inputProps={{ maxLength: 1 }} />
+                            {inputCode.map((data, index) => (
+                                <TextField
+                                    type="text"
+                                    // eslint-disable-next-line react/no-array-index-key
+                                    key={index}
+                                    name={`input-field-${index}`}
+                                    inputMode="numeric"
+                                    onChange={(event) => handleInputChange(event, index)}
+                                    inputProps={{ maxLength: 1, pattern: '/^d+$/' }}
+                                    required
+                                    onFocus={(e) => e.target.select}
+                                    autoFocus={index === 0}
+                                />
+                            ))}
                         </Box>
-                        <SubmitButton text="white">Bekreft</SubmitButton>
                     </Form>
                     <LinkText onClick={() => navigate(-1)}>Gå tilbake</LinkText>
                 </SectionWrapper>
