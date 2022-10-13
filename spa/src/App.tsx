ea2f10cef4d7a9073af8e1030e5c7468ef6b7870
glensorbo <glensorbo@gmail.com>
2022-10-13 15:53:13 +0200

    feat: :sparkles: user logged in after browser reload

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { LocationInfo } from './pages/LocationInfo';
@@ -16,29 +17,47 @@ import { Onboarding } from './features/userRegistration/components/Onboarding';
 import { ChangePassword } from './features/login/components/ChangePassword';
 import { ResetPassword } from './features/login/components/ResetPassword';
 import { ChangeEmail } from './features/profile/ChangeEmail';
+import { useStateDispatch } from './hooks/useRedux';
+import { userService } from './features/userRegistration/services/user.services';
 
-const App = () => (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/location-info" element={<LocationInfo />} />
-            <Route path="/location-registration" element={<LocationRegistration />} />
-            <Route path="/location-approval-panel" element={<AdminPanel />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/change-password" element={<ChangePassword />} />
-            <Route path="/change-email" element={<ChangeEmail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/user-registration" element={<UserRegistration />} />
-            <Route path="/email-input" element={<EmailInput />} />
-            <Route path="/email-confirmation" element={<EmailConfirmation />} />
-            <Route path="/personal-info" element={<PersonalInfo />} />
-            <Route path="/create-password" element={<CreatePassword />} />
-            <Route path="/personalization" element={<Personalization />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-        <CustomizedSnackbars />
-    </Router>
-);
+const App = () => {
+    const dispatch = useStateDispatch();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const parsedUser = localStorage.getItem('user');
+        let user = null;
+        if (parsedUser) {
+            user = JSON.parse(parsedUser);
+        }
+        if (user && token) {
+            dispatch(userService.getUser(user.id));
+        }
+    }, [dispatch]);
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/location-info" element={<LocationInfo />} />
+                <Route path="/location-registration" element={<LocationRegistration />} />
+                <Route path="/location-approval-panel" element={<AdminPanel />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/change-password" element={<ChangePassword />} />
+                <Route path="/change-email" element={<ChangeEmail />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/user-registration" element={<UserRegistration />} />
+                <Route path="/email-input" element={<EmailInput />} />
+                <Route path="/email-confirmation" element={<EmailConfirmation />} />
+                <Route path="/personal-info" element={<PersonalInfo />} />
+                <Route path="/create-password" element={<CreatePassword />} />
+                <Route path="/personalization" element={<Personalization />} />
+                <Route path="/onboarding" element={<Onboarding />} />
+                <Route path="/reset-password" element={<ResetPassword />} />
+            </Routes>
+            <CustomizedSnackbars />
+        </Router>
+    );
+};
 
 export default App;
