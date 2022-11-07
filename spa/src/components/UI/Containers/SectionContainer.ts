05b8b673109691f2e939a07bb429301fb71d32de
Jostein <Jostein-skare@hotmail.com>
2022-11-07 11:53:12 +0100

    style: :lipstick: removed unnecessary padding on home page
    removed padding from SectionContainer.ts to remove whitespace on top and bottom from the home page

--- spa/src/components/UI/Containers/SectionContainer.ts
+++ spa/src/components/UI/Containers/SectionContainer.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const SectionContainer = styled.section`
     width: 100%;
     height: 100%;
-    padding: 1rem;
     display: flex;
     flex-direction: column;
     align-items: center;
