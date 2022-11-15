e59a28d7446a536747eb482355b9abea13e1880c
glensorbo <glensorbo@gmail.com>
2022-11-15 08:11:59 +0100

    refactor: :recycle: made esLint happy

--- spa/src/utils/sleep.ts
+++ spa/src/utils/sleep.ts
@@ -1,7 +1,7 @@
-export const sleep = (time: number) => {
-    return new Promise<void>((resolve, reject) => {
+export const sleep = (time: number) =>
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    new Promise<void>((resolve, reject) => {
         setTimeout(() => {
             resolve();
         }, time);
     });
-};
